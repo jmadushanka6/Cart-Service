edfd7b18e7d9de0398d4e8bd87c9bba78f1693e2
jmadushanka <janitha.pm6@gmail.com>
2025-11-16 00:47:07 +0100

    fix: provide rule registry

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -4,10 +4,11 @@ import { CartController } from './cart.controller';
 import { CartEngineService } from './cart.engine/cart.engine.service';
 import { RuleLoader } from './cart.engine/rule.loader';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RuleRegistry } from './cart.engine/rule.registry';
 
 @Module({
   imports: [ConfigModule],
-  providers: [CartService, CartEngineService, RuleLoader, ConfigService],
+  providers: [CartService, CartEngineService, RuleLoader,RuleRegistry, ConfigService],
   controllers: [CartController]
 })
 export class CartModule {}
