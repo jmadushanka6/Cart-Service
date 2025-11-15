8a11f1e10cb8761d50674bd7b76e8a40a163f7f2
jmadushanka <janitha.pm6@gmail.com>
2025-11-16 00:56:32 +0100

    feat: cart calculation boilerplate added

--- src/cart/cart.engine/cart.engine.service.ts
+++ src/cart/cart.engine/cart.engine.service.ts
@@ -1,8 +1,36 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { RuleLoader } from './rule.loader';
+import { RuleRegistry } from './rule.registry';
+import { CartRequest, CartResponse } from '../dto/cart.dto';
+import { EngineContext } from '../dto/engine.dto';
 
 @Injectable()
-export class CartEngineService implements OnModuleInit {
-    onModuleInit() {
-        // throw new Error('Method not implemented.');
+export class CartEngineService {
+    constructor(private ruleLoader: RuleLoader, private ruleRegistry: RuleRegistry) {
+
     }
+
+    async calculateCart(cart: CartRequest): Promise<any> {
+
+        const subtotal: number = cart.items.reduce((sum, i) => sum + (i.price * i.quantity), 0);
+
+        let context: EngineContext = {
+            items: cart.items,
+            subtotal,
+            totalDiscount: 0,
+            totalAfterDiscounts: subtotal,
+            vatAmount: 0,
+            totalPayable: subtotal,
+        };
+        const rules = this.ruleLoader.getRules();
+
+        return ({
+            discountsApplied: context.totalDiscount,
+            totalAfterDiscounts: context.totalAfterDiscounts,
+            vatAmount: context.vatAmount,
+            totalPayable: context.totalPayable,
+        });
+    }
+
 }
+
